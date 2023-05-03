498095834e569145dc57b38d775d1b43e10f611d
luxagen <hello@luxagen.com>
2023-05-03 14:47:07 +0100

    [TEMP] directly address rk3

--- src/main.rs
+++ src/main.rs
@@ -123,7 +123,7 @@ fn fsnode_open(path: &str) -> Box<dyn std::io::Read>
 		{return Box::new(std::fs::File::open(path).unwrap());}
 
 	let mut rk = Command::new("sudo");
-	rk.args(["rk","-urei",path]);
+	rk.args(["/vcs/bin/rk3","-urei",path]);
 	rk.stdin(Stdio::null());
 	rk.stdout(Stdio::piped());
 
