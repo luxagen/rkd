7479c8f6732c3d36c62ffd73b700d5b8d3ebdb5f
luxagen <hello@luxagen.com>
2023-07-29 19:54:25 +0100

    rk3 -> rk

--- src/main.rs
+++ src/main.rs
@@ -126,7 +126,7 @@ fn fsnode_open(path: &str) -> Box<dyn std::io::Read>
 		{return Box::new(std::fs::File::open(path).unwrap());}
 
 	let mut rk = Command::new("sudo");
-	rk.args(["/vcs/bin/rk3","-urei",path]);
+	rk.args(["/vcs/bin/rk","-urei",path]);
 	rk.stdin(Stdio::null());
 	rk.stdout(Stdio::piped());
 
