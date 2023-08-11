a5acd364b9ffe864cd0b122d380cd75bf6cfb9f0
luxagen <hello@luxagen.com>
2023-08-11 16:34:42 +0100

    Removed absolute path from RK invocation

--- src/main.rs
+++ src/main.rs
@@ -126,7 +126,7 @@ fn fsnode_open(path: &str) -> Box<dyn std::io::Read>
 		{return Box::new(std::fs::File::open(path).unwrap());}
 
 	let mut rk = Command::new("sudo");
-	rk.args(["/vcs/bin/rk","-urei",path]);
+	rk.args(["rk","-re",path]);
 	rk.stdin(Stdio::null());
 	rk.stdout(Stdio::piped());
 
