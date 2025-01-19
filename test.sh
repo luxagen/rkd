831ba327c62af4d2d1fd2fe342347a689589cc58
luxagen <hello@luxagen.com>
2025-01-19 19:10:52 +0000

    [DEBUG] TEST CASE

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cargo build -r
+echo GOOD
+target/release/rkd <(cd /btr/int/hash-logs && sudo git show f9778302c05b85f1b92f77c04c0deb5d09af7f9b~1:md5.txt) <(cd /btr/int/hash-logs && sudo git show f9778302c05b85f1b92f77c04c0deb5d09af7f9b:md5.txt) 2>&1 | sort | tee test-good.txt
+echo BAD
+sudo target/release/rkd / / 2>&1 | sort | tee test-bad.txt
