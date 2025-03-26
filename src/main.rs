948f778ef5b21dd4ff263eb3e48e22d4b484b38c
luxagen <hello@luxagen.com>
2025-03-26 21:24:38 +0000

    Move matching: exclude empty files so they show up as RM+CR

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,8 @@ struct Hash
 	bytes: [u8;16],
 }
 
+const EMPTY_HASH: Hash = Hash { bytes: [0xd4, 0x1d, 0x8c, 0xd9, 0x8f, 0x00, 0xb2, 0x04, 0xe9, 0x80, 0x09, 0x98, 0xec, 0xf8, 0x42, 0x7e] };
+
 impl Hash
 {
 	fn new(from: &str) -> Self
@@ -427,8 +429,10 @@ impl RKD
 
 		debug_assert_eq!(self.sides.len(),2);
 
-		for obj in self.hashes.values()
+		for (hash,obj) in self.hashes.iter()
 		{
+			if hash == &EMPTY_HASH {continue}
+
 			// Build a reference list for RHS, excluding done items (i.e. make a "to report on" list)
 			let mut pathsR = obj.sides[1].paths.iter()
 				.filter_map(|nodeR| if !nodeR.is_done() {Some(*nodeR)} else {None})
@@ -806,5 +810,25 @@ impl<'a, Item> VecIterator<'a, Item>
 	fn curr(&mut self) -> Option<&'a Item>
 	{
 		self.vector.get(self.index as usize)
+	}
+}
+
+#[cfg(test)]
+mod tests
+{
+	use super::*;
+
+	#[test]
+	fn test_empty_hash_value_matches_string_representation()
+	{
+		// The EMPTY_HASH constant has a hardcoded value
+		// This test verifies that it matches what would be parsed from the string
+		let expected = EMPTY_HASH;
+		let from_string = Hash::new("d41d8cd98f00b204e9800998ecf8427e");
+
+		assert_eq!(expected, from_string, "EMPTY_HASH hardcoded value should match the value parsed from string");
+
+		// Confirm the debug representation also matches
+		assert_eq!(format!("{:?}", expected), format!("{:?}", from_string));
 	}
 }
